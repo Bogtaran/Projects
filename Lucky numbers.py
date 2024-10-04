cd1d86df674ae4af48f83687537d8b5ffbe2c1ff
Bogdan <bogtaran87@yandex.ru>
2024-10-04 21:47:07 +0600

    Окончательный вариант.

--- Lucky numbers.py
+++ Lucky numbers.py
@@ -1,19 +1,44 @@
+'''
+Счастливое число определено следующим процессом. Начиная с некоторого положительного целого числа, замените число
+суммой квадратов его цифр и повторяйте процесс до тех пор, пока число не будет равным одному(на чем все и остановится)
+или оно будет циклиться бесконечно. Если цикл конечен, то изначальное число называется счастливым.
+Найдите первые 8 счастливых чисел.
+'''
+
+
 def main():
-    lucky_numbers = []
-    for i in range(10, 20):
-        num = sum_squares_digits(i)
-        if num == 1 or num == 10 or num == 13:
-            lucky_numbers.append(i)
-        elif len(str(num)) >= 2:
-            sum_squares_digits(num)
-    return lucky_numbers
+    lucky_num = []
+    count = 0
+    for i in range(10, 1000):
+        if lucky_number(i):
+            lucky_num.append(i)
+            count += 1
+            if count == 8:
+                return print(lucky_num)
 
 
 def sum_squares_digits(i):
     """Данная функция находит сумму квадратов цифр числа"""
     i = str(i)
     numbers = sum([int(j) ** 2 for j in i])
+    if numbers in [2, 3, 4, 5, 6, 7, 8, 9]:
+        return 0
     return numbers
+
+
+def lucky_number(i):
+    """Данная функция выясняет счастливое число или нет"""
+    num = sum_squares_digits(i)
+    if num == 1 or num == 10 or num == 13:
+        return True
+    elif num == 0:
+        return False
+    else:
+        try:
+            # num = sum_squares_digits(num)
+            lucky_number(num)
+        except RecursionError:
+            return False
 
 
 if __name__ == '__main__':
